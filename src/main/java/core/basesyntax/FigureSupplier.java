be04fcc0f2de5e0bd1b0e1544f016266209ef6e9
Marcin Laube <laubemarcin2gmail.com>
2026-06-15 13:55:24 +0200

    figure supplier

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,39 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private static final int FIGURE_COUNT = 5;
+    private static final double DEFAULT_RADIUS = 10.0;
+    private static final String DEFAULT_COLOR = "white";
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        String color = colorSupplier.getRandomColor();
+        int figureType = random.nextInt(FIGURE_COUNT);
+
+        // Losujemy wymiary od 1.0 do 11.0
+        double val1 = random.nextDouble() * 10 + 1;
+        double val2 = random.nextDouble() * 10 + 1;
+        double val3 = random.nextDouble() * 10 + 1;
+
+        switch (figureType) {
+            case 0:
+                return new Square(val1, color);
+            case 1:
+                return new Circle(val1, color);
+            case 2:
+                return new Rectangle(val1, val2, color);
+            case 3:
+                return new RightTriangle(val1, val2, color);
+            case 4:
+            default:
+                return new IsoscelesTrapezoid(val1, val2, val3, color);
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
+    }
+}
---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private double sideA;
+    private double sideB;
+    private double height;
+
+    public IsoscelesTrapezoid(double sideA, double sideB, double height, String color) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return ((sideA + sideB) * height) / 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: " + getArea()
+                + " sq. units, sideA: " + sideA
+                + " units, sideB: " + sideB
+                + " units, height: " + height
+                + " units, color: " + getColor());
+    }
+}
